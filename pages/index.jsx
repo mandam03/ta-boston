8d600d2a0ad68641d01611be7bbe72290c84e112
mandam03 <mmiha_49@yahoo.com>
2022-11-03 22:01:45 +0200

    03.11.2022- next-js

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,5 +5,12 @@ export default function Home() {
     hello boston<i className="fa-solid fa-house"></i>
 
   <img src="/images/logo.jpg" alt="Alt" />
+
+  <button className="button button-primary">Button Primary</button>
+  <button className="button button-primary button-large">Button Primary Large</button>
+
+  <a className="button button-primary">Button Primary</a>
+  <a className="button button-primary button-large">Button Primary Large</a>
+
   </div>;
 }
